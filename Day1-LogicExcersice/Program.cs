6bf78cd7acf7dfb2b60dad3912459fb0ccdec3e0
Kesid Dewa <kesiddewa600@gmail.com>
2025-05-13 11:39:35 +0700

    logic

---
+++ Day1-LogicExcersice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            for (int i = 1; i <= num; i++) {
+                if (i % 5 == 0 && i % 3 == 0) {
+                    Console.WriteLine("foobar, ");
+                }
+                else if (i % 3 == 0) {
+                    Console.WriteLine("foo, ");
+                }
+                else if (i % 5 == 0) {
+                    Console.WriteLine("bar, ");
+                }
+                else {
+                    Console.WriteLine(i + ", ");
+                }
+            }
+        }
+    }
+}
