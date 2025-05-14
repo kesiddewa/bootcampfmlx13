9aea07d0acfa3d5e355762f9b8808352aa63b7f3
Kesid Dewa <kesiddewa600@gmail.com>
2025-05-14 10:09:44 +0700

    day2

---
+++ Day2/Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿class Bank {
+    private double _Amount;
+    public double Amount {
+        get {
+            return _Amount;
+        }
+        set {
+            _Amount = value;
+        }
+    }
+}
+
+class Program {
+    static void Main(string[] args)
+    {
+        Bank bank = new Bank();
+        bank.Amount = 124155.12;
+        Console.WriteLine(bank.Amount);
+    }
+}
