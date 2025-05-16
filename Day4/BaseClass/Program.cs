2f27c3f67c059275e69acdfbeb0464a8479180d7
Kesid Dewa <kesiddewa600@gmail.com>
2025-05-16 13:58:48 +0700

    Day 4

---
+++ Day4/BaseClass/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Animal
+{
+    public string Name;
+
+    public Animal(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal speaks...");
+    }
+}
+
+public class Dog : Animal
+{
+    public Dog(string name) : base(name) // Memanggil constructor Animal
+    {
+    }
+
+    public override void Speak()
+    {
+        base.Speak(); // Memanggil method Speak dari kelas Animal
+        Console.WriteLine($"{Name} says: Woof!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dog myDog = new Dog("Buddy");
+        myDog.Speak();
+    }
+}
---
+++ Day4/NestedType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class OuterClass
+{
+    public string Name = "Saya OuterClass";
+
+    public void ShowOuter()
+    {
+        Console.WriteLine("Method OuterClass.");
+    }
+
+    public class NestedClass
+    {
+        public void ShowNested()
+        {
+            Console.WriteLine("Method NestedClass.");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        OuterClass outer = new OuterClass();
+        outer.ShowOuter();
+        Console.WriteLine(outer.Name);
+
+        OuterClass.NestedClass nested = new OuterClass.NestedClass();
+        nested.ShowNested();
+    }
+}
