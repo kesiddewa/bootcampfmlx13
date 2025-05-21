88b88137b68c78a70ae09193f442b269ee00bb0e
Kesid Dewa <kesiddewa600@gmail.com>
2025-05-21 10:58:24 +0700

    day 7

---
+++ Day7/LogicExercise2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 107;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    if (i % 7 == 0)
+                    {
+                        System.Console.Write("foobarjazz, ");
+                    }
+                    else
+                    {
+
+                        Console.Write("foobar, ");
+                    }
+                }
+
+                else if (i % 3 == 0)
+                {
+                    if (i % 7 == 0 && i % 3 == 0)
+                    {
+                        System.Console.Write("foojazz, ");
+                    }
+                    else
+                    {
+
+                        Console.Write("foo, ");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    if (i % 7 == 0 && i % 5 == 0)
+                    {
+                        System.Console.Write("barjazz, ");
+                    }
+                    else
+                    {
+
+                        Console.Write("bar, ");
+                    }
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("jazz, ");
+
+                }
+                else
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+        }
+    }
+}
