d553f7467da40ac24dad03d6fcd3818532738701
Kesid Dewa <kesiddewa600@gmail.com>
2025-06-04 13:49:32 +0700

    Day 11

---
+++ Day11/StreamReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+namespace FileHandlinDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // //Creating a new input stream i.e.  MyFile.txt and opens it
+            // StreamReader sr = new StreamReader(@"C:\Users\Kesid Dewa\MyFile.txt");
+
+            // Console.WriteLine("Content of the File");
+
+            // // This is used to specify from where to start reading input stream
+            // sr.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            // // To read line from input stream
+            // string str = sr.ReadLine();
+
+            // // To read the whole file line by line
+            // while (str != null)
+            // {
+            //     Console.WriteLine(str);
+            //     str = sr.ReadLine();
+            // }
+            // Console.ReadLine();
+
+            // // to close the stream
+            // sr.Close();
+            // Console.ReadKey();
+
+
+
+            string FilePath = @"C:\Users\Kesid Dewa\MyFil.txt";
+            string data;
+            FileStream fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            using (StreamReader streamReader = new StreamReader(fileStream))
+            {
+                data = streamReader.ReadToEnd();
+            }
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day11/StreamWriter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+namespace FileHandlinDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // Code for user input
+
+            // // This will create a file named MyFile2.txt at the specified location i.e. in the D Drive
+            // // Here we are using the StreamWriter constructor which takes the string path as an argument to create an instance of StreamWriter class
+            // StreamWriter sw = new StreamWriter(@"C:\Users\Kesid Dewa\MyFile2.txt");
+
+            // //Asking user to enter the text that we want to write into the MyFile.txt file
+            // Console.WriteLine("Enter the Text that you want to write on File");
+
+            // // To read the input from the user
+            // string str = Console.ReadLine();
+
+            // // To write the data into the stream
+            // sw.Write(str);
+
+            // // Clears all the buffers
+            // sw.Flush();
+
+            // // To close the stream
+            // sw.Close();
+            // Console.ReadKey();
+
+
+            // Code for save variable data into file
+            string file = @"C:\Users\Kesid Dewa\MyTextFile1.txt";
+            int a, b, result;
+            a = 15;
+            b = 20;
+            result = a + b;
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.Write("Sum of {0} + {1} = {2}", a, b, result);
+            }
+            Console.WriteLine("Saved");
+            Console.ReadKey();
+        }
+    }
+}
